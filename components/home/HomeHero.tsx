a8fbf40382ca3eaeff5c49a5aa6d3d6734268958
sasha-astiadi <sasha@incubaid.com>
2025-07-31 19:07:18 +0200

    edit jome

--- components/home/HomeHero.tsx
+++ components/home/HomeHero.tsx
@@ -2,25 +2,13 @@ export function HomeHero() {
   return (
     <div className="relative">
       <div className="mx-auto max-w-7xl lg:grid lg:grid-cols-12 lg:gap-x-8 lg:px-8">
-        <div className="px-6 pt-10 pb-24 sm:pb-32 lg:col-span-7 lg:px-0 lg:pt-40 lg:pb-48 xl:col-span-6">
+        <div className="px-6 pt-10 pb-24 lg:col-span-7 lg:px-0 lg:py-32 xl:col-span-6">
           <div className="mx-auto max-w-lg lg:mx-0">
-            <img
-              alt="Your Company"
-              src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=600"
-              className="h-11"
-            />
-
             <div className="hidden sm:mt-32 sm:flex lg:mt-16">
-              <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-500 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                Anim aute id magna aliqua ad ad non deserunt sunt.{' '}
-                <a href="#" className="font-semibold whitespace-nowrap text-indigo-600">
-                  <span aria-hidden="true" className="absolute inset-0" />
-                  Read more <span aria-hidden="true">&rarr;</span>
-                </a>
               </div>
             </div>
-            <h1 className="mt-24 text-5xl font-semibold tracking-tight text-pretty text-gray-900 sm:mt-10 sm:text-7xl">
-              Data to enrich your business
+            <h1 className="mt-24 text-5xl font-semibold tracking-tight text-pretty text-white sm:mt-10 sm:text-7xl">
+              Build, Swap, and Earn Effortlessly.
             </h1>
             <p className="mt-8 text-lg font-medium text-pretty text-gray-500 sm:text-xl/8">
               Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
@@ -39,14 +27,14 @@ export function HomeHero() {
             </div>
           </div>
         </div>
-        <div className="relative lg:col-span-5 lg:-mr-8 xl:absolute xl:inset-0 xl:left-1/2 xl:mr-0">
+        <div className="relative m w-full overflow-hidden rounded-lg  lg:col-span-5 lg:mt-0 xl:col-span-6">
           <img
-            alt=""
-            src="https://images.unsplash.com/photo-1487017159836-4e23ece2e4cf?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2102&q=80"
-            className="aspect-3/2 w-full bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full"
+            src="/images/home/window.png"
+            alt="App screenshot"
+            className="absolute inset-0 h-full w-full object-contain"
           />
-        </div>
       </div>
+    </div>
     </div>
   )
 }
