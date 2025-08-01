157298164f7182ee6370689a88940cd449a350c7
sasha-astiadi <sasha@incubaid.com>
2025-08-01 18:55:28 +0200

    add features

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { HomeHero } from '../components/home/HomeHero';
+import { HomeFeatures } from '../components/home/HomeFeatures';
 
 export default function Home() {
   return (
     <>
       <HomeHero />
+      <HomeFeatures />
     </>
   )
 }
